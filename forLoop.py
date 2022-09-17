760eb2c2b4c7fd05ba812152ba61410cf4ed14fe
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-09-17 22:28:34 +0600

    for loop in python

---
+++ forLoop.py
@@ -0,0 +1,12 @@
+
+
+friends = ['Nur', 'Sahab', 'Riad', 'Shahin']
+for i in friends :
+    print(i)
+
+
+num = int(input('Enter a number :'))
+for i in range(11) :
+    if i > 0 :
+        print(num, ' x ', i, ' : ', num*i)
+
