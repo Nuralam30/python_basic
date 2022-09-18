f9acfd97709dc906de3c19d7d0b61329df3bdeb6
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-09-18 19:14:16 +0600

    even or odd number

--- arithmatic.py
+++ arithmatic.py
@@ -6,8 +6,14 @@ sum = x + y
 sub = sum - x
 mul = x * y
 div = y / x
+div2 = y // x
+mod = y % x
+pow = x ** 2
 
 print(sum)
 print(sub)
 print(mul)
 print(div)
+print(div2)
+print(mod)
+print(pow)
---
+++ numbers.py
@@ -0,0 +1,7 @@
+
+num = 1
+max = 100
+
+while num <= max :
+    print(num)
+    num = num + 1
