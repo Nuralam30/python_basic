3912992b615507cac05209b624e0a6deb5dd3fc2
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-09-18 20:05:34 +0600

    odd numbers add in a range

--- addNumbers.py
+++ addNumbers.py
@@ -3,8 +3,23 @@ num = 1
 max = 10
 sum = 0
 
+# while num <= max :
+#     sum = sum + num
+#     num = num + 1
+# print(sum)
+
+
+# # even numbers added
+# while num <= max :
+#     if num % 2 == 0 :
+#         sum = sum + num
+#     num = num + 1
+# print(sum)
+
+
+# odd numbers added
 while num <= max :
-    sum = sum + num
+    if num % 2 != 0 :
+        sum = sum + num
     num = num + 1
 print(sum)
-
