c5aa6be1d76c637e43e2d8275ff9ab8ddd1e0ae7
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-09-21 17:52:13 +0600

    recursive method in sum of series

--- sumOfSeries.py
+++ sumOfSeries.py
@@ -1,9 +1,19 @@
 
-x = int(input('Enter the last number of the series :'))
-i = 1
-sum = 0
+n = int(input('Enter the last number of the series :'))
+# i = 1
+# sum = 0
 
-while i <= x :
-    sum = sum + i*i
-    i += 1
-print(sum)
+# while i <= n :
+#     sum = sum + i*i
+#     i += 1
+# print(sum)
+
+
+# recursive method for series
+def sum(n):
+    if n == 1:
+        return 1
+    else:
+        return  n*n + sum(n-1)
+
+print(sum(n))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,2 @@
-# even numbers added
-# while num <= max :
-#     if num % 2 == 0 :
-#         sum = sum + num
-#     num = num + 1
-# print(sum)
+
+    else:
